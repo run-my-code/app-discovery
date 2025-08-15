f03e92f67565a9df6864d9744aa9c3f301d080f0
takwa01 <boubaker.takwa@gmail.com>
2025-08-15 17:10:19 +0200

    enable eureka server and display its dashboard

--- src/main/java/com/example/eureka/app_discovery/AppDiscoveryApplication.java
+++ src/main/java/com/example/eureka/app_discovery/AppDiscoveryApplication.java
@@ -2,10 +2,13 @@ package com.example.eureka.app_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AppDiscoveryApplication {
-
+	// @EnableEurekaServer : This is to enable a Spring Boot application to function
+	// as a Netflix Eureka
 	public static void main(String[] args) {
 		SpringApplication.run(AppDiscoveryApplication.class, args);
 	}
