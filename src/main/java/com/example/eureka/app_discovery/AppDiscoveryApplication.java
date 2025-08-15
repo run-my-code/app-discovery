206e380c5fbeeec74e822bff3eb7aeb42bda051c
takwa01 <boubaker.takwa@gmail.com>
2025-08-15 16:52:42 +0200

    create eureka server project

---
+++ src/main/java/com/example/eureka/app_discovery/AppDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka.app_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eureka/app_discovery/AppDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka.app_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
